3e70334ea7ee4b0f62e822b07a36f5d40183b19a
Chethan A C <acchethan15@gmail.com>
2024-08-19 16:24:56 +0530

    Create Love calculator.js

---
+++ Love calculator.js
@@ -0,0 +1,6 @@
+prompt("What is your name?");
+prompt("What is your partner name?");
+
+var loveScore = Math.random() × 100;
+loveScore = Math.floor(loveScore) + 1;
+alert("Your love score is" + loveScore + "%");
