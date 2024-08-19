9e1872655b67fe128e8130edfbc3a878bff7f2c4
Chethan A C <acchethan15@gmail.com>
2024-08-19 20:22:08 +0530

    Create Arrays.js

---
+++ Arrays.js
@@ -0,0 +1,9 @@
+var guestName = ["abhi" , "bhuvi" , "chini"]
+var guestList = prompt("What is your name?"]
+guestName.includes(guestList);
+
+if (guestName.includes(guestList) {
+  alert("Welcome");
+} else {
+  alert("Sorry,may be next time");
+}
