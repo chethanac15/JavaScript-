16c4fae73c152e6a8e30a32e4a944b7b18386007
Chethan A C <acchethan15@gmail.com>
2024-08-23 21:38:28 +0530

    Create DOM.js

---
+++ DOM.js
@@ -0,0 +1,26 @@
+List of codes
+
+document.getElementsbyTagName("li");
+document.getElementsbyLastName("btn");
+document.getElementbyId("title");
+document.querySelector("h1");
+
+*Codes for separation of concern*
+document.querySelector("button").classList;
+document.querySelector("button").classList.add("newclass");
+document.querySelector("button").classList.remove("newclass");
+document.querySelector("button").classList.toggle("newclass");
+
+
+*Codes forText manipulation*
+
+document.getElementsbyId("title").innerHTML="Hello";
+document.getElementsbyId("title").textContent="Bye";
+
+
+*Codes for Elements manipulation*
+document.querySelector("a");
+document.querySelector("a").attributes;
+document.querySelector("a"). getAttributes("href");
+document.querySelector("a").setAttributes("href","https://.......");
+
