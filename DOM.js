5b9823d10f70058e461e59c8683406f9656f984b
Chethan A C <acchethan15@gmail.com>
2024-09-15 21:55:33 +0530

    Update DOM.js

--- DOM.js
+++ DOM.js
@@ -5,20 +5,20 @@ document.getElementsbyLastName("btn");
 document.getElementbyId("title");
 document.querySelector("h1");
 
-*Codes for separation of concern*
+//Codes for separation of concern
 document.querySelector("button").classList;
 document.querySelector("button").classList.add("newclass");
 document.querySelector("button").classList.remove("newclass");
 document.querySelector("button").classList.toggle("newclass");
 
 
-*Codes forText manipulation*
+//Codes forText manipulation
 
 document.getElementsbyId("title").innerHTML="Hello";
 document.getElementsbyId("title").textContent="Bye";
 
 
-*Codes for Elements manipulation*
+//Codes for Elements manipulation
 document.querySelector("a");
 document.querySelector("a").attributes;
 document.querySelector("a"). getAttributes("href");
