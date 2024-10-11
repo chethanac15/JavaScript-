9fa345f6736a7be0222a261ba11f1a8b2f369f5e
Chethan A C <acchethan15@gmail.com>
2024-10-11 23:04:11 +0530

    Update Uppercase.js

--- Uppercase.js
+++ Uppercase.js
@@ -1,4 +1,4 @@
-prompt ("What is your name");
+prompt ("What is your name ?");
 var name = prompt("What is your name");
 var firstChar = name.slice(0,1);
 var upperCasefirstChar = firstChar.toUpperCase();
