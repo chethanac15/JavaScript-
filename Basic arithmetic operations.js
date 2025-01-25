aee40ff15308f57892ddaeffe24133389c474e35
Chethan A C <acchethan15@gmail.com>
2025-01-25 23:48:55 +0530

    Update Basic arithmetic operations.js

--- Basic arithmetic operations.js
+++ Basic arithmetic operations.js
@@ -1,4 +1,4 @@
-prompt("How old is your dog");
+prompt("How old is your dog ?");
 var dogAge = prompt("How old is your dog");
 var humanAge = ((dogAge - 2) * 4) +21;
 alert("your dog is" + " " + "humanAge" + " " year old of human age");
